ee96a1750e242a2fb7d32c9798e58575e4a8ed4d
tarun <edupugantitarunvenkatasai@gmail.com>
2022-06-02 13:12:25 +0530

    game completed

---
+++ OneDrive/Desktop/web development html/Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,80 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattren = [];
+var userClickedPattern = [];
+var level=0;
+var start=false;
+
+
+$(document).on("keypress",function(){
+    if(start===false)
+    {
+        $("h1").text("Level"+level);
+        start=true;
+        nextSequence();
+        //level=0;
+    }
+     
+});
+
+
+function checkAnswer(currentLevel){
+      if(userClickedPattern[currentLevel]===gamePattren[currentLevel]){
+          if(userClickedPattern.length===gamePattren.length){
+             setTimeout(function(){nextSequence();},1000)  ;
+
+          }
+      }
+      else{
+          playSound("wrong");
+          $("body").addClass("game-over");
+          $("h1").text("Game Over, Press Any Key to Restart");
+          setTimeout(function(){
+            $("body").removeClass("game-over");
+          },200);
+          
+          startOver();
+      }
+}
+
+function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level"+level);
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColour=buttonColors[randomNumber];
+    gamePattren.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    
+    
+}
+
+$(".btn").on("click",function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+    
+})
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100)
+
+}
+
+
+function startOver(){
+     gamePattren = [];
+userClickedPattern = [];
+level=0;
+start=false;
+}
